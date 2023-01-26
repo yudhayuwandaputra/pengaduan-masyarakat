b8c34fb19b8348c34d63ebbc9799d4f0d28ec7bb
Yudha Yuwanda Putra <yudhayuwandaputra@gmail.com>
2023-01-26 22:16:22 +0700

    Second Commit

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -11,7 +11,7 @@
 
     
         <!-- Earnings (Monthly) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-5">
+        <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
